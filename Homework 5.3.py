39164dd3245e98dbdfc5819fdd6441066dac948a
MARIABODOWSKAYA\Мария Бодовская <mashabodovskaya@gmail.com>
2021-02-07 19:21:57 +0300

    Add ДЗ(2)

--- Homework 5.3.py
+++ Homework 5.3.py
@@ -1,21 +1,15 @@
 """
-Два натуральных числа называют дружественными,
-если каждое из них равно сумме всех делителей другого,
-кроме самого этого числа. Найти все пары дружественных чисел,
-лежащих в диапазоне от 200 до 300.
+Дано число 28340928374 посчитать сумму цифр этого числа (генератор списков)
 """
 
-number = 0
-for elem in range(200, 300):
-    if elem != number:
-        number1 = 0
-        for i in range(1, elem // 2 + 1):
-            if elem % i == 0:
-                number1 += i
-        number2 = 0
-        for df in range(1, number1 // 2 + 1):
-            if number1 % df == 0:
-                number2 += df
-        if number2 == elem != number1:
-            print(elem, number1)
-            f = number1
+from functools import reduce
+
+numbers = 28340928374
+new_list = [int(i) for i in str(numbers)]
+
+
+def do_sum(x1, x2):
+    return x1 + x2
+
+
+print(reduce(do_sum, new_list))
