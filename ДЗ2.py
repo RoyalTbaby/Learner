b3e07ad3e796688e7112639dd9def8929d0dae3d
MARIABODOWSKAYA\Мария Бодовская <mashabodovskaya@gmail.com>
2021-02-08 14:07:31 +0300

    add homework2

---
+++ ДЗ2.py
@@ -0,0 +1,15 @@
+"""
+Дано число 28340928374 посчитать сумму цифр этого числа (генератор списков) ЧЕРЕЗ REDUCE
+"""
+
+from functools import reduce
+
+numbers = 28340928374
+new_list = [int(i) for i in str(numbers)]
+
+
+def do_sum(x1, x2):
+    return x1 + x2
+
+
+print(reduce(do_sum, new_list))
